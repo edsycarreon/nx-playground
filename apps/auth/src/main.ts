bcdb23f432e4f00b9b2d0fdcb22836353a595a83
Edson Carreon <edsonjaybcarreon@gmail.com>
2025-10-29 15:09:42 +0800

    versioning configruation

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
